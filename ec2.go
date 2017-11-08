babcdbd7f64255e90d09b86feb3f3a11c5b385f6
yhidetoshi <hyajima.1014@gmail.com>
2017-11-09 00:20:13 +0900

    リソース ec2 rdsを追加

---
+++ ec2.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/olekukonko/tablewriter"
+)
+
+const (
+	EC2 = "ec2"
+)
+
+// EC2リソース接続用
+func AwsEC2Client(profile string, region string) *ec2.EC2 {
+	//ec2Client := ec2.New(session.New(), &aws.Config{Region: aws.String("ap-northeast-1")})
+	var config aws.Config
+	if profile != "" {
+		creds := credentials.NewSharedCredentials("", profile)
+		config = aws.Config{Region: aws.String(region), Credentials: creds}
+	} else {
+		config = aws.Config{Region: aws.String(region)}
+	}
+	ses := session.New(&config)
+	ec2Client := ec2.New(ses)
+
+	return ec2Client
+}
+
+
+func StopEC2Instances(ec2Client *ec2.EC2, ec2Instances []*string) {
+	params := &ec2.StopInstancesInput{
+		InstanceIds: ec2Instances,
+	}
+	res, err := ec2Client.StopInstances(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	for _, r := range res.StoppingInstances {
+		fmt.Printf("%s stopped", *r.InstanceId)
+	}
+}
+
+func StartEC2Instances(ec2Client *ec2.EC2, ec2Instances []*string) {
+	params := &ec2.StartInstancesInput{
+		InstanceIds: ec2Instances,
+	}
+	res, err := ec2Client.StartInstances(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	for _, r := range res.StartingInstances {
+		fmt.Printf("%s started", *r.InstanceId)
+	}
+}
+
+
+func TerminateEC2Instances(ec2Client *ec2.EC2, ec2Instances []*string) {
+	params := &ec2.TerminateInstancesInput{
+		InstanceIds: ec2Instances,
+	}
+	res, err := ec2Client.TerminateInstances(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	for _, r := range res.TerminatingInstances {
+		fmt.Printf("%s terminated", *r.InstanceId)
+	}
+}
+
+func ListEC2Instances(ec2Client *ec2.EC2, ec2Instances []*string) {
+	params := &ec2.DescribeInstancesInput{
+		InstanceIds: ec2Instances,
+	}
+	res, err := ec2Client.DescribeInstances(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	allInstances := [][]string{}
+
+	for _, resInfo := range res.Reservations {
+		for _, instanceInfo := range resInfo.Instances {
+			var tagName string
+			for _, tagInfo := range instanceInfo.Tags {
+				if *tagInfo.Key == "Name" {
+				tagName = *tagInfo.Value
+				}
+			}
+			// PublicIpAddressがNULLの場合の例外処理
+			if instanceInfo.PublicIpAddress == nil {
+				instanceInfo.PublicIpAddress = aws.String("NULL")
+			}
+
+			// PrivateIpAddressがNULLの場合の例外処理
+			if instanceInfo.PrivateIpAddress == nil {
+				instanceInfo.PrivateIpAddress = aws.String("NULL")
+			}
+
+			instance := []string{
+				tagName,
+				*instanceInfo.InstanceId,
+				*instanceInfo.InstanceType,
+				*instanceInfo.Placement.AvailabilityZone,
+				*instanceInfo.PublicIpAddress,
+				*instanceInfo.PrivateIpAddress,
+				*instanceInfo.State.Name,
+			}
+			allInstances = append(allInstances, instance)
+    	}
+	}
+	OutputFormat(allInstances, EC2)
+}
+
+func ControlEC2Instances(ec2Client *ec2.EC2, ec2Instances []*string, operation string) {
+	ListEC2Instances(ec2Client, ec2Instances)
+
+	fmt.Print("Do you control Instance ?")
+	var stdin string
+	fmt.Scan(&stdin)
+
+	switch stdin {
+	case "y", "Y", "yes":
+		switch operation {
+	case "start":
+		fmt.Println("Start EC2 instance")
+		StartEC2Instances(ec2Client, ec2Instances)
+	case "stop":
+		fmt.Println("Stop EC2 instance")
+		StopEC2Instances(ec2Client, ec2Instances)
+	case "terminate":
+		fmt.Println("Terminate EC2 instance")
+		TerminateEC2Instances(ec2Client, ec2Instances)
+	}
+	case "n", "N", "no":
+		fmt.Println("Exit ...!")
+		os.Exit(0)
+	default:
+		fmt.Println("Exit ...!")
+		os.Exit(0)
+	}
+}
+
+func GetEC2InstanceIds(ec2Client *ec2.EC2, ec2Instances string) []*string {
+	splitedInstances := strings.Split(ec2Instances, ",")
+	res, err := ec2Client.DescribeInstances(nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	var instanceIds []*string
+	for _, s := range splitedInstances {
+		for _, r := range res.Reservations {
+			for _, i := range r.Instances {
+				for _, t := range i.Tags {
+					if *t.Key == "Name" {
+						if *t.Value == s {
+							instanceIds = append(instanceIds, aws.String(*i.InstanceId))
+						}
+					}
+				}
+				if *i.InstanceId == s {
+					instanceIds = append(instanceIds, aws.String(*i.InstanceId))
+				}
+			}
+		}
+	}
+	return instanceIds
+}
+
+// 出力のフォーマット(外部のモジュール利用)
+func OutputFormat(data [][]string, resourceType string) {
+	table := tablewriter.NewWriter(os.Stdout)
+	if resourceType == EC2 {
+		table.SetHeader([]string{"tag:Name", "InstanceId", "InstanceType", "AvailabilityZone", "PrivateIp", "PublicIp", "Status"})
+	}
+
+	for _, value := range data {
+		table.Append(value)
+	}
+	table.Render()
+}
+
---
+++ rds.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rds"
+	"github.com/olekukonko/tablewriter"
+)
+
+const (
+	RDS = "rds"
+)
+
+
+// RDSリソース接続用
+func AwsRDSClient(profile string, region string) *rds.RDS {
+	var config aws.Config
+	if profile != "" {
+		creds := credentials.NewSharedCredentials("", profile)
+		config = aws.Config{Region: aws.String(region), Credentials: creds}
+	} else {
+		config = aws.Config{Region: aws.String(region)}
+	}
+	ses := session.New(&config)
+	rdsClient := rds.New(ses)
+
+	return rdsClient
+}
+
+
+func StopRDSInstances(rdsClient *rds.RDS, rdsInstances *string) {
+	params := &rds.StopDBInstanceInput{
+		DBInstanceIdentifier: rdsInstances,
+	}
+	_, err := rdsClient.StopDBInstance(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	} else {
+		fmt.Println("Success...!")
+	}
+}
+
+
+func StartRDSInstances(rdsClient *rds.RDS, rdsInstances *string) {
+	params := &rds.StartDBInstanceInput{
+		DBInstanceIdentifier: rdsInstances,
+	}
+	_, err := rdsClient.StartDBInstance(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	} else {
+		fmt.Println("Success...!")
+	}
+}
+
+
+func ListRDSInstances(rdsClient *rds.RDS, rdsInstances *string) {
+	params := &rds.DescribeDBInstancesInput{
+		DBInstanceIdentifier: rdsInstances,
+	}
+	res, err := rdsClient.DescribeDBInstances(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	allDBInstances := [][]string{}
+
+	for _, resInfo := range res.DBInstances {
+		instance := []string{
+			*resInfo.DBInstanceIdentifier,
+			*resInfo.DBInstanceClass,
+			*resInfo.DBInstanceStatus,
+			*resInfo.Engine,
+			*resInfo.EngineVersion,
+			*resInfo.MasterUsername,
+			*resInfo.DBName,
+			*resInfo.AvailabilityZone,
+		}
+		allDBInstances = append(allDBInstances, instance)
+	}
+	//fmt.Println(allDBInstances)
+	OutputFormat(allDBInstances, RDS)
+}
+
+
+func ControlRDSInstances(rdsClient *rds.RDS, rdsInstances *string, operation string) {
+	ListRDSInstances(rdsClient, rdsInstances)
+
+	fmt.Print("Do you control DBInstance ?")
+	var stdin string
+	fmt.Scan(&stdin)
+
+	switch stdin {
+	case "y", "Y", "yes":
+		switch operation {
+		case "start":
+			fmt.Println("start RDS instance")
+			StartRDSInstances(rdsClient, rdsInstances)
+		case "stop":
+			StopRDSInstances(rdsClient, rdsInstances)
+		}
+	case "n", "N", "no":
+		fmt.Println("Exit ...!")
+		os.Exit(0)
+	default:
+		fmt.Println("Exit ...!")
+		os.Exit(0)
+	}
+}
+
+
+func GetRDSInstanceName(rdsClient *rds.RDS, rdsInstances string) *string {
+	splitedInstances := strings.Split(rdsInstances, ",")
+	res, err := rdsClient.DescribeDBInstances(nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	var instanceName *string
+	for _, s := range splitedInstances {
+		for _, res := range res.DBInstances {
+			if *res.DBInstanceIdentifier == s {
+				instanceName = aws.String(rdsInstances)
+			}
+		}
+	}
+	return instanceName
+}
+
+
+// 出力のフォーマット(外部のモジュール利用)
+func OutputFormat(data [][]string, resourceType string) {
+	table := tablewriter.NewWriter(os.Stdout)
+	if resourceType == RDS {
+		table.SetHeader([]string{"DBName", "InstanceType", "Status", "Engine", "EngineVersion", "MasterUsername", "DBName", "AvailabilityZone"})
+	}
+
+	for _, value := range data {
+		table.Append(value)
+	}
+	table.Render()
+}
