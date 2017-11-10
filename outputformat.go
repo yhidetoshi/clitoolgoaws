7e41aeb191c67a3dc446bc337cda1c8c7ef31439
yhidetoshi <hyajima.1014@gmail.com>
2017-11-10 17:30:51 +0900

    ELBにインスタンスを登録・登録解除の機能を実装

--- outputformat.go
+++ outputformat.go
@@ -8,16 +8,20 @@ import (
 func OutputFormat(data [][]string, resourceType string) {
 	table := tablewriter.NewWriter(os.Stdout)
 
-	if resourceType == EC2 {
-		table.SetHeader([]string{"tag:Name", "InstanceId", "InstanceType", "AvailabilityZone", "PrivateIp", "PublicIp", "Status"})
-	}
-
-	if resourceType == RDS {
-		table.SetHeader([]string{"DBName", "InstanceType", "Status", "Engine", "EngineVersion", "MasterUsername", "DBName", "AvailabilityZone"})
+	switch resourceType{
+		case EC2:
+			table.SetHeader([]string{"tag:Name", "InstanceId", "InstanceType", "AvailabilityZone", "PrivateIp", "PublicIp", "Status"})
+		case RDS:
+			table.SetHeader([]string{"DBName", "InstanceType", "Status", "Engine", "EngineVersion", "MasterUsername", "DBName", "AvailabilityZone"})
+	    case ELB:
+	    	table.SetHeader([]string{"ELB_Name", "Scheme", "VPCId", "DNSName"})
+	    case ELB_INS:
+	    	table.SetHeader([]string{"BackEnd_INstance"})
 	}
 
 	for _, value := range data {
 		table.Append(value)
 	}
+
 	table.Render()
 }
